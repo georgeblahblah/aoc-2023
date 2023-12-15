6ea2f979e9129fd0f07f01fa876fc3be86bf9f77
George B <705427+georgeblahblah@users.noreply.github.com>
2023-12-15 12:37:13 +0000

    Day 3 part 2

--- aoc-scala/day3/src/main/scala/Main.scala
+++ aoc-scala/day3/src/main/scala/Main.scala
@@ -1,16 +1,16 @@
 import common.file._
 import scala.util.matching.Regex.Match
 
-// val input = """467..114..
-// ...*......
-// ..35..633.
-// ......#...
-// 617*......
-// .....+.58.
-// ..592.....
-// ......755.
-// ...$.*....
-// .664.598.."""
+val input = """467..114..
+...*......
+..35..633.
+......#...
+617*......
+.....+.58.
+..592.....
+......755.
+...$.*....
+.664.598.."""
 
 @main def main(fileName: String) = {
   val fileContents = readFile(fileName)
@@ -19,30 +19,51 @@ import scala.util.matching.Regex.Match
 }
 
 def part1(input: String) = {
-  val symbolRegex = """([^\d\.\s])""".r
+  val symbolRegex = raw"([^\d\.\s])".r
   val splitInput = input.split("\n")
 
   val symbolMap = (for
-    rowCount <- 0 to splitInput.length - 1
+    rowCount <- 0 until splitInput.length
     row = splitInput(rowCount)
     symbol <- symbolRegex.findAllMatchIn(row)
-  yield ((rowCount, symbol.start) -> symbol.matched)).toMap
+  yield (Position(rowCount, symbol.start) -> symbol.matched)).toMap
 
-  val numberRegex = """\d+""".r
+  val numberRegex = raw"\d+".r
   (for
-    rowCount <- 0 to splitInput.length - 1
+    rowCount <- 0 until splitInput.length
     row = splitInput(rowCount)
     numberMatch <- numberRegex.findAllMatchIn(row)
   yield
     if (for
         scanRow <- rowCount - 1 to rowCount + 1
         scanCol <- numberMatch.start - 1 to numberMatch.end
-        symbol = symbolMap.get((scanRow, scanCol))
+        symbol = symbolMap.get(Position(scanRow, scanCol))
         if symbol.isDefined
       yield true).contains(true)
     then numberMatch.matched.toInt
     else 0).sum
+}
+
+def part2(input: String) = {
+  val gearRegex = raw"\*".r
+  val numberRegex = raw"\d+".r
+  val splitInput = input.split("\n")
+  (for
+    (line, idx) <- splitInput.zipWithIndex
+    gearMatch <- gearRegex.findAllMatchIn(line)
+    above = if idx > 0 then splitInput(idx - 1) else ""
+    below = if idx < splitInput.length - 1 then splitInput(idx + 1) else ""
+    numberMatches = numberRegex.findAllMatchIn(above) ++ numberRegex
+      .findAllMatchIn(below) ++ numberRegex.findAllMatchIn(line)
+    gearNumbers = numberMatches
+      .filter((numberMatch) =>
+        (numberMatch.start - 1).to(numberMatch.end).contains(gearMatch.start)
+      )
+      .map(_.matched.toInt)
+      .toSeq
+    if gearNumbers.length == 2
+  yield gearNumbers.foldLeft(1)(_ * _)).sum
 
 }
 
-def part2(input: String) = {}
+case class Position(x: Int, y: Int)
